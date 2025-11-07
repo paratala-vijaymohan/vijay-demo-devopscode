bec73fe18122021b9b5a7b8b4c4486d06396350d
paratala.vijaymohan <vijayp@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-07 05:46:11 +0000

    updated yaml file

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -10,18 +10,19 @@ resource "aws_instance" "example" {
   tags = {
     "name"= "${var.vm-name}-${count.index}"
   }
+# depends_on = [ tls_private_key.example , aws_key_pair.example ]
 
-  provisioner "remote-exec" {
-    inline = [ 
-      "sudo dnf install git httpd -y",
-      "mkdir -p hello/terraform"
-     ]
-  }
-  connection {
-    type = "ssh"
-    user = "ec2-user"
-    host = self.public_ip
-    timeout = "3m"
-    private_key = tls_private_key.example.private_key_pem
-  }
+  # provisioner "remote-exec" {
+  #   inline = [ 
+  #     "sudo dnf install git httpd -y",
+  #     "mkdir -p hello/terraform"
+  #    ]
+  # }
+  # connection {
+  #   type = "ssh"
+  #   user = "ec2-user"
+  #   host = self.public_ip
+  #   timeout = "3m"
+  #   private_key = tls_private_key.example.private_key_pem
+  # }
 }
